96f31b46ba6f1c31ffa60a3f4c9d29c86c394ea8
harshit-kumar78 <harshit3551@gmail.com>
2023-05-02 00:55:26 +0530

    seller form created and json-server set

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
-  styleUrls: ['./seller-auth.component.css']
+  styleUrls: ['./seller-auth.component.css'],
 })
 export class SellerAuthComponent {
-
+  onSubmit(formVal: object) {
+    console.log(formVal);
+  }
 }
